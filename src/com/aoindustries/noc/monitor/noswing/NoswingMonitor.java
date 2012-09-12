3e4484b93d718e304521c1b0d6fe8ecd5107eb62
Dan Armstrong <dan@aoindustries.com>
2012-09-12 09:34:05 +0000

    Switching to use wrapper.

--- src/com/aoindustries/noc/monitor/noswing/NoswingMonitor.java
+++ src/com/aoindustries/noc/monitor/noswing/NoswingMonitor.java
@@ -6,31 +6,48 @@
 package com.aoindustries.noc.monitor.noswing;
 
 import com.aoindustries.noc.monitor.common.Monitor;
-import com.aoindustries.noc.monitor.common.RootNode;
-import java.io.IOException;
+import com.aoindustries.noc.monitor.wrapper.WrappedMonitor;
+import com.aoindustries.util.IdentityKey;
 import java.rmi.RemoteException;
-import java.sql.SQLException;
-import java.util.Locale;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.Callable;
 import javax.swing.SwingUtilities;
 
 /**
  * @author  AO Industries, Inc.
  */
-public class NoswingMonitor implements Monitor {
+public class NoswingMonitor extends WrappedMonitor {
 
     static void checkNoswing() {
         if(SwingUtilities.isEventDispatchThread()) throw new AssertionError("Running in Swing event dispatch thread");
     }
 
-    final private Monitor wrapped;
+    private static final Map<IdentityKey<Monitor>,NoswingMonitor> cache = new HashMap<IdentityKey<Monitor>,NoswingMonitor>();
 
-    public NoswingMonitor(Monitor wrapped) {
-        this.wrapped = wrapped;
+    /**
+     * One unique NoswingMonitor is created for each monitor (by identity equals).
+     */
+    public static NoswingMonitor getInstance(Monitor wrapped) throws RemoteException {
+        if(wrapped instanceof NoswingMonitor) return (NoswingMonitor)wrapped;
+        IdentityKey<Monitor> key = new IdentityKey<Monitor>(wrapped);
+        synchronized(cache) {
+            NoswingMonitor server = cache.get(key);
+            if(server==null) {
+                server = new NoswingMonitor(wrapped);
+                cache.put(key, server);
+            }
+            return server;
+        }
+    }
+
+    private NoswingMonitor(Monitor wrapped) {
+        super(wrapped);
     }
 
     @Override
-    public RootNode login(Locale locale, String username, String password) throws RemoteException, IOException, SQLException {
+    protected <T> T call(Callable<T> callable, boolean allowRetry) throws RemoteException {
         checkNoswing();
-        return new NoswingRootNode(wrapped.login(locale, username, password));
+        return super.call(callable, allowRetry);
     }
 }
